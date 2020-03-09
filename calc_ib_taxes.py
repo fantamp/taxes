de40593a667d1c3d8685613c3d8a879f62556b56
Andrey Mescheryakov <fant.amp@gmail.com>
2020-03-09 23:01:22 +0300

    Print profits report

--- calc_ib_taxes.py
+++ calc_ib_taxes.py
@@ -20,41 +20,57 @@ class Trade:
         self.sold_buyings = []
 
     def __str__(self):
-        return '<Trade {} {} {} {} {} {}>'.format(self.date, self.kind, self.symbol, self.amount, self.price, self.sold_buyings)
+        return '<Trade {} {} {} {} {}>'.format(self.date, self.kind, self.symbol, self.amount, self.price)
 
     __repr__ = __str__
 
 
 def do_the_thing(trades):
     sales = [copy.copy(t) for t in trades if t.kind == 'sell']
-    byings = [copy.copy(t) for t in trades if t.kind == 'buy']
+    buyings = [copy.copy(t) for t in trades if t.kind == 'buy']
     for s in sales:
         amount_to_find = s.amount
         while amount_to_find > 0:
-            b_from = byings[0]
-            b = copy.copy(b_from)
-            if b_from.amount > amount_to_find:
-                b.amount = amount_to_find
-                b_from.amount -= amount_to_find
-            else:
-                b.amount = b_from.amount
-                byings = byings[1:]
-            amount_to_find -= b.amount
-            s.sold_buyings.append(b)
-    return sales
+            for i in range(len(buyings)):
+                b_from = buyings[i]
+                if b_from.amount <= 0 or b_from.symbol != s.symbol:
+                    continue
+                b = copy.copy(b_from)
+                if b_from.amount > amount_to_find:
+                    b.amount = amount_to_find
+                else:
+                    b.amount = b_from.amount
+                b_from.amount -= b.amount
+                amount_to_find -= b.amount
+                s.sold_buyings.append(b)
+                if amount_to_find <= 0:
+                    break
+        buyings = [b for b in buyings if b.amount > 0]
+    return sales, buyings
 
 
 usd_rub_exchange_rate_for_date = {}
 
 
 def get_usd_rub_exchange_rate_for_date(d):
+    def d2key(d):
+        return d.strftime('%d.%m.%Y')
     if len(usd_rub_exchange_rate_for_date) <= 0:
         with open('usd_rub.dat') as f:
+            prev_date = None
+            prev_rate = None
             for line in f:
                 date_str, rate_str = line.split('\t')
-                rate = decimal.Decimal(rate_str.replace(',', '.').replace(' ', ''))
-                usd_rub_exchange_rate_for_date[date_str.strip()] = rate
-    d_str = d.strftime('%d.%m.%Y')
+                curr_rate = decimal.Decimal(rate_str.replace(',', '.').replace(' ', ''))
+                curr_date = datetime.datetime.strptime(date_str, '%d.%m.%Y')
+                if prev_date is not None and (curr_date-prev_date).days > 1:
+                    for delta in range((curr_date-prev_date).days - 1):
+                        interm_date = prev_date + datetime.timedelta(days=delta+1)
+                        usd_rub_exchange_rate_for_date[d2key(interm_date)] = prev_rate
+                usd_rub_exchange_rate_for_date[d2key(curr_date)] = curr_rate
+                prev_date = curr_date
+                prev_rate = curr_rate
+    d_str = d2key(d)
     return usd_rub_exchange_rate_for_date[d_str]
 
 
@@ -89,7 +105,33 @@ def load_trades_from_dir(dirname):
 
 
 def main():
-    print("hi!")
+    def to_rub_str(t):
+        rate = get_usd_rub_exchange_rate_for_date(t.date)
+        return '{:0.2f} RUB ({} * {} * {})'.format(t.amount * t.price * rate, t.amount, t.price, rate)
+
+    trades = load_trades_from_dir('ib_reports')
+    print('All trades:')
+    for t in trades:
+        print(t)
+    print()
+    sales, buyings_left = do_the_thing(trades)
+    print('Sales:')
+    for t in sales:
+        inc_rub = get_usd_rub_exchange_rate_for_date(t.date) * t.amount * t.price
+        print(t)
+        print('    Income: {}'.format(to_rub_str(t)))
+        print('    What was sold:')
+        exp_rub = decimal.Decimal(0)
+        for sold in t.sold_buyings:
+            print('        *', sold, to_rub_str(sold))
+            exp_rub += get_usd_rub_exchange_rate_for_date(sold.date) * sold.amount * sold.price
+        print('    Buy = {:0.2f} RUB // Sell = {:0.2f} RUB // Profit = {:0.2f} RUB'.format(exp_rub, inc_rub, inc_rub-exp_rub))
+    print()
+    print('Buyings left:')
+    if len(buyings_left) <= 0:
+        print('(none)')
+    for t in buyings_left:
+        print(t)
 
 
 class T(unittest.TestCase):
@@ -104,15 +146,18 @@ class T(unittest.TestCase):
             trade = Trade(date=d[0], kind=d[1], symbol=d[2], amount=d[3], price=d[4])
             trades.append(trade)
         self.assertEqual(len(trades), 4)
-
-        sales = do_the_thing(trades)
-        self.assertEqual(len(sales), 2)
-
+        sales, b_left = do_the_thing(trades)
         print(sales)
+        print(b_left)
+        self.assertEqual(len(sales), 2)
+        self.assertEqual(len(b_left), 1)
+
 
     def testRatesDb(self):
         rate = get_usd_rub_exchange_rate_for_date(datetime.datetime(2018, 7, 27, 9, 33, 38))
         self.assertEqual(rate, decimal.Decimal('62.9471'))
+        rate = get_usd_rub_exchange_rate_for_date(datetime.datetime(2020, 1, 5, 9, 33, 38))
+        self.assertEqual(rate, decimal.Decimal('61.9057'))
 
     def testIbReportLine(self):
         line = 'Trades,Data,Order,Stocks,USD,AAPL,"2019-01-03, 09:54:22",1,143.25,142.19,-143.25,-1,144.25,0,-1.06,O'
